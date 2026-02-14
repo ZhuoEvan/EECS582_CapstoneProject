fc390e149e074cb697ff4ef759f931a18bc13ee6
IanFoeh <ifoehrweiser@ku.edu>
2026-02-14 12:38:27 -0600

    You can now move the rat across the stage

--- capstone-project/character.gd
+++ capstone-project/character.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
-@export var speed: float
+@export var health : int
+@export var damage : int
+@export var speed : float
+ 
 func _process(delta: float) -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	position += direction * delta * speed
