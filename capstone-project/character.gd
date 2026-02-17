c2004075320726afb58b4ccbc08a98ba8659fffb
ZhuoEvan <156927177+ZhuoEvan@users.noreply.github.com>
2026-02-17 11:51:10 -0600

    Merge Branches & Added Comments
    Title says all.

--- capstone-project/character.gd
+++ capstone-project/character.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 
+# ============================================= #
+# STATISTIC VARIABLES							#
+# ============================================= #
 @export var health : int
 @export var damage : int
 @export var speed : float
 
+# ============================================= #
+# State											#
+# ============================================= #
 enum State { IDLE, WALK, ATTACK }
 var state: State = State.IDLE
 var attack_type := ""
 
+
+# ============================================= #
+# Methods										#
+# ============================================= #
+
+#Process Method
 func _process(_delta: float) -> void:
 	handle_input()
 	if state != State.ATTACK:
@@ -15,10 +27,12 @@ func _process(_delta: float) -> void:
 	handle_attacks()
 	move_and_slide()
 
+#Handling Input Method
 func handle_input() -> void:
 	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = direction * speed
-
+	
+	#Determine if it is Light ATK & Heavy ATK
 	if can_attack() and Input.is_action_just_pressed("light_attack"):
 		attack_type = "light_attack"
 		state = State.ATTACK
@@ -26,16 +40,19 @@ func handle_input() -> void:
 		attack_type = "heavy_attack"
 		state = State.ATTACK
 
+#Handling Movement Input Method
 func handle_movement() -> void:
 	if velocity.length() == 0:
 		state = State.IDLE
 	else:
 		state = State.WALK
 
+#Handling Attack Input Method
 func handle_attacks() -> void:
 	if state == State.ATTACK:
 		print(attack_type)
 		state = State.IDLE
 
+#Attack Condition Helper Method
 func can_attack() -> bool:
 	return state == State.IDLE or state == State.WALK
