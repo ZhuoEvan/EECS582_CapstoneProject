9758c05d10cb442c87944819a0bbcc428e6be3a2
zchen12345 <157060764+zchen12345@users.noreply.github.com>
2026-03-17 16:29:01 -0500

    update menu
    updated menu with a level selection method

--- capstone-project/main_menu(control).gd
+++ capstone-project/main_menu(control).gd
@@ -7,32 +7,29 @@
 # 	Game is open										#
 # Postcondition: 										#
 # 	Player being able to go various setting of the game#
-# Creation date: 03/27/26								#
+# Creation date: 02/27/26								#
 # ----------------------------------------------------- #
-# Last modifed date:03/27/26							#
+# Last modifed date:03/17/26							#
 # Changes: 												#
-#	implemented a rough draft of the menu
+#	implemented the level selection menu and level 1
 # ===================================================== #
 extends Control
 
 @onready var level_select = $LevelSelect
+@onready var main_menu = $ColorRect
 #Ready Method
 func _ready():
 	level_select.visible = false
 
 #set level select panel to true so player can do level select
-#This function is currently not connected to LevelSelect, since there is no multiple level
-func _on_PlayButton_pressed():
+func _on_play_pressed():
 	level_select.visible = true
+	main_menu.visible = false
 
 #set level select panel to false as player have back out of the level select menu
-#This function is currently not connected to LevelSelect, since there is no multiple level
-func _on_BackButton_pressed():
+func _on_back_pressed():
 	level_select.visible = false
-
-#currently comment out, but the basic idea is for each level created a button, cread a press function
-#used get_tree() to change the scence to the corrsponding level
-#func _on_Level1Button_pressed():
+	main_menu.visible = true
 
 #non-functional, since it is not connected to anything
 #func _on_SkillButton_pressed():
@@ -46,6 +43,6 @@ func _on_BackButton_pressed():
 #func _on_CreditsButton_pressed():
 	#get_tree().change_scene_to_file(")
 
-#currently play will go straight to the demo level, however, it should go to level select if multiple level is createdd
-func _on_play_pressed() -> void:
+#level 1 button, currently connected to the main game
+func _on_Level1_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_game.tscn")
